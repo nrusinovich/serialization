48d3dc4779aee4c716c0700dffab7e5a27245ea9
Nadzeya Rusinovich <Nadzeya_Rusinovich@epam.com>
2017-02-20 13:49:30 +0300

    init

---
+++ Serialization/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    [XmlType("book")]
+   public class Book
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+        [XmlElement("ibsn")]
+        public string Ibsn { get; set; }
+        [XmlElement("author")]
+        public string Author { get; set; }
+        [XmlElement("publisher")]
+        public string Publisher { get; set; }
+        [XmlElement("title")]
+        public string Title { get; set; }
+        [XmlElement("genre")]
+        public Genre Genre { get; set; }
+        [XmlElement("description")]
+        public string Description{ get; set; }
+        [XmlElement("publish_date")]
+        public DateTime PublishDate { get; set; }
+        [XmlElement("register_date")]
+        public DateTime RegisterDate { get; set; }
+        public Book()
+        {
+
+        }
+        public Book(string id = "", string ibsn = "", string author = "", string title = "", string description = "", string publisher = "", Genre genre = Genre.Computer, string pdate = "2001-01-01", string rdate = "2001-01-01")
+        {
+            Id = id;
+            Ibsn = ibsn;
+            Author = author;
+            Title = title;
+            Description = description;
+            Publisher = publisher;
+            PublishDate = DateTime.Parse(pdate).Date;
+            RegisterDate = DateTime.Parse(rdate).Date;
+            Genre = genre;
+
+        }
+
+    }
+  
+   public enum Genre
+    {
+        Fantasy,
+        Computer,
+        Romance
+    }
+}
---
+++ Serialization/Catalog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+     public class Catalog1 : List<Book>
+    {
+        public Catalog1()
+        {
+
+        }
+        public Catalog1(List<Book> books)
+        {
+           AddRange(books);
+        }
+    }
+}
